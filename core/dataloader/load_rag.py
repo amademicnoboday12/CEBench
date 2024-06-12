203653d45e689e25518e9423dcf1cfb0584526c3
wenbo1212 <sunwenbo1212@outlook.com>
2024-06-12 09:31:26 +0200

    change project structure

---
+++ core/dataloader/load_rag.py
@@ -0,0 +1,54 @@
+from qdrant_client import QdrantClient, models
+from qdrant_client.models import Distance, VectorParams
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Qdrant
+from private_embedding import embeddings
+
+
+def load_knowledge_base(rag_collection, doc_path, quantization=None, chunk_size=512):
+    overlap = chunk_size // 8
+    client = QdrantClient()
+
+    if quantization == "pq":
+        quantization_conf = models.ProductQuantization(
+            product=models.ProductQuantizationConfig(
+                compression=models.CompressionRatio.X4, always_ram=True
+            )
+        )
+    elif quantization == "scalar":
+        quantization_conf = models.ScalarQuantization(
+            scalar=models.ScalarQuantizationConfig(
+                type=models.ScalarType.INT8, always_ram=True
+            )
+        )
+    else:
+        quantization_conf = None
+    loader = DirectoryLoader(
+        doc_path,
+        glob="**/*.txt",
+        show_progress=True,
+        use_multithreading=True,
+        loader_cls=TextLoader,
+    )
+    documents = loader.load()
+    print(f"Loaded {len(documents)} documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=overlap, separators=["\n\n", "\n"]
+    )
+    flatmap = lambda f, xs: [y for ys in xs for y in f(ys)]
+    chunks = flatmap(text_splitter.split_documents, documents)
+    dimensions = len(embeddings.embed_query("this is a test"))
+    client.delete_collection(collection_name=rag_collection)
+    client.create_collection(
+        collection_name=rag_collection,
+        vectors_config=VectorParams(size=dimensions, distance=Distance.COSINE),
+        quantization_config=quantization_conf,
+    )
+    qdrant = Qdrant(
+        client=client, embeddings=embeddings, collection_name=rag_collection
+    )
+    qdrant.add_documents(chunks)
+    print(
+        f"Added {len(chunks)} chunks, {len(documents)} documents to collection {rag_collection}"
+    )
--- post_processing.py
+++
@@ -1,8 +0,0 @@
-def post_processing_mental(response):
-    import re
-    score_search = re.search(r'Total score: (\d{1,2})', response)
-    if score_search and 0 <= int(score_search.group(1)) <= 24:
-        score = score_search.group(1)
-    else:
-        score = "-1"
-    return score
